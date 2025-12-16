e212050c8a86f7bb38337277b5beb585d8f7907e
fahad4920 <fahadsajid492@gmail.com>
2025-12-16 12:19:24 +0500

    reset both

--- server.js
+++ server.js
@@ -18,34 +18,34 @@ app.get("/", (req, res) => {
 });
 
 // ===== INSERT ECG DATA =====
-// app.post("/ecg", async (req, res) => {
-//   try {
-//     const { patientId, value } = req.body;
+app.post("/ecg", async (req, res) => {
+  try {
+    const { patientId, value } = req.body;
 
-//     await pool.query(
-//       "INSERT INTO ecg_data (patient_id, value) VALUES ($1, $2)",
-//       [patientId, value]
-//     );
+    await pool.query(
+      "INSERT INTO ecg_data (patient_id, value) VALUES ($1, $2)",
+      [patientId, value]
+    );
 
-//     res.send("Saved");
-//   } catch (err) {
-//     console.log(err);
-//     res.status(500).send("Error");
-//   }
-// });
+    res.send("Saved");
+  } catch (err) {
+    console.log(err);
+    res.status(500).send("Error");
+  }
+});
 
-// // ===== GET ECG DATA =====
-// app.get("/ecg/:id", async (req, res) => {
-//   try {
-//     const result = await pool.query(
-//       "SELECT * FROM ecg_data WHERE patient_id=$1 ORDER BY time DESC LIMIT 300",
-//       [req.params.id]
-//     );
+// ===== GET ECG DATA =====
+app.get("/ecg/:id", async (req, res) => {
+  try {
+    const result = await pool.query(
+      "SELECT * FROM ecg_data WHERE patient_id=$1 ORDER BY time DESC LIMIT 300",
+      [req.params.id]
+    );
 
-//     res.json(result.rows);
-//   } catch (err) {
-//     res.status(500).send("Error");
-//   }
-// });
+    res.json(result.rows);
+  } catch (err) {
+    res.status(500).send("Error");
+  }
+});
 
 app.listen(3000, () => console.log("Server running on port 3000"));
