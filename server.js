431f0b77c85932be524d2244ec9001063cc0a2c3
fahad4920 <fahadsajid492@gmail.com>
2026-01-04 12:48:24 +0500

    fix

--- server.js
+++ server.js
@@ -28,7 +28,10 @@ app.post("/ecg", async (req, res) => {
     );
 
     res.send("Saved");
-  } 
+  } catch (err) {
+    console.log(err);
+    res.status(500).send("Error");
+  }
 });
 
 // ===== GET ECG DATA =====
