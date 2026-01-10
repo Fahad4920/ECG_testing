75cd4faeea705ed7c50aa92d2a15acb0fcb381b1
fahad4920 <fahadsajid492@gmail.com>
2026-01-10 12:56:13 +0500

    ...

--- server.js
+++ server.js
@@ -21,7 +21,7 @@ app.get("/", (req, res) => {
 // ===== INSERT ECG DATA =====
 app.post("/ecg", async (req, res) => {
   try {
-    const { patientId, value } = req.body;
+    
 
     await pool.query(
       "INSERT INTO ecg_data (patient_id, value) VALUES ($1, $2)",
