d1c968eff1edda885a643c0beb8ed3215cc1287f
fahad4920 <fahadsajid492@gmail.com>
2026-01-18 13:08:02 +0500

    ...

--- server.js
+++ server.js
@@ -38,6 +38,7 @@ app.get("/ecg/:id", async (req, res) => {
       "SELECT * FROM ecg_data WHERE patient_id=$1 ORDER BY time DESC LIMIT 300",
       [req.params.id]
     );
+    
     res.json(result.rows);
   } catch (err) {
     res.status(500).send("Error");
