a16103441075beeff902adc2fcba0e266e9c1c16
fahad4920 <fahadsajid492@gmail.com>
2026-01-24 09:59:44 +0500

    commit and test

--- server.js
+++ server.js
@@ -42,6 +42,6 @@ app.post("/ecg", async (req, res) => {
 //   } catch (err) {
 //     res.status(500).send("Error");
 //   }
-});
+// });
 
 app.listen(3000, () => console.log("Server running on port 3000"));
